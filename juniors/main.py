1369eee6ed686d409b2eb0e6b4ed19140bff1b9c
jeperico <joaoeperico@gmail.com>
2026-07-28 22:03:32 -0300

    feat: remove /

--- juniors/main.py
+++ juniors/main.py
@@ -47,10 +47,6 @@ ingressos = {
 
 available = ingressos["total"] - ingressos["sold"]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.post("/reservations", status_code=201)
 async def handle_reservation(request: Request):
     payload = await request.json()
